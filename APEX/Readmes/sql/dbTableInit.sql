553b674d43536c5090ed49a84251d28e173cadb5
Tevin <78318984+tevinric@users.noreply.github.com>
2025-05-05 12:00:59 +0200

    Update dbTableInit.sql

--- APEX/Readmes/sql/dbTableInit.sql
+++ APEX/Readmes/sql/dbTableInit.sql
@@ -1,3 +1,5 @@
+-- SQL file to recreate the logs table with all columns including the new ones
+-- First drop the table if it exists to avoid errors
 IF OBJECT_ID('[dbo].[logs]', 'U') IS NOT NULL
 BEGIN
     DROP TABLE [dbo].[logs];
@@ -7,7 +9,7 @@ END
 CREATE TABLE [dbo].[logs] (
     [id] varchar(50) NULL,
     [eml_id] varchar(MAX) NULL,
-    [internet_message_id] varchar(MAX) NULL,
+    [internet_message_id] varchar(900) NULL, -- Limited to 900 for indexing
     [dttm_rec] datetime NULL,
     [dttm_proc] datetime NULL,
     [eml_to] varchar(MAX) NULL,
@@ -26,10 +28,13 @@ CREATE TABLE [dbo].[logs] (
     [sts_routing] text NULL,
     [tat] float NULL,
     [end_time] datetime NULL,
-    [apex_intervention] text NULL,
-    [apex_top_categories] varchar(MAX) NULL
+    [apex_intervention] text NULL, -- New column for tracking AI intervention
+    [apex_top_categories] varchar(MAX) NULL -- New column for storing top 3 categories
 );
 
--- Add an index on internet_message_id for faster lookups
-CREATE INDEX [IX_logs_internet_message_id] ON [dbo].[logs] ([id]);
-CREATE INDEX [IX_logs_eml_id] ON [dbo].[logs] ([internet_message_id]);
+-- Add an index on id and internet_message_id for faster lookups
+CREATE INDEX [IX_logs_id] ON [dbo].[logs] ([id]);
+CREATE INDEX [IX_logs_internet_message_id] ON [dbo].[logs] ([internet_message_id]);
+
+-- Grant permissions if needed
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON [dbo].[logs] TO [your_user_or_role];
