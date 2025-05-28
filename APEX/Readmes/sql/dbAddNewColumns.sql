efbf6efe9bb2f2796612d829bcbd45d56cf36dea
Tevin <78318984+tevinric@users.noreply.github.com>
2025-05-28 11:02:56 +0200

    Update dbAddNewColumns.sql

--- APEX/Readmes/sql/dbAddNewColumns.sql
+++ APEX/Readmes/sql/dbAddNewColumns.sql
@@ -1,10 +1,2 @@
-ALTER TABLE [dbo].[logs] ADD
-    [region_used] VARCHAR(10) NULL,
-    [gpt_4o_prompt_tokens] INT NULL,
-    [gpt_4o_completion_tokens] INT NULL,
-    [gpt_4o_total_tokens] INT NULL,
-    [gpt_4o_cached_tokens] INT NULL,
-    [gpt_4o_mini_prompt_tokens] INT NULL,
-    [gpt_4o_mini_completion_tokens] INT NULL,
-    [gpt_4o_mini_total_tokens] INT NULL,
-    [gpt_4o_mini_cached_tokens] INT NULL;
+ALTER TABLE [dbo].[logs]
+ADD [auto_response_sent] varchar(50) NULL;
